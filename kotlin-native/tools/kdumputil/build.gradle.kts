e3eb63c8cedf5bb99a6f1dab74d3f0408f40b9ef
Michał Pociecha-Łoś <micapolos@google.com>
2024-06-10 11:48:25 +0200

    gradle subproject + reformat

--- kotlin-native/tools/kdumputil/build.gradle.kts
+++ kotlin-native/tools/kdumputil/build.gradle.kts
@@ -1,27 +1,13 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.9.10"
+    kotlin("jvm")
     application
 }
 
-group = "org.jetbrains.kdump"
+group = "org.jetbrains.kdumputil"
 version = "1.0.0"
-
-repositories {
-    mavenCentral()
-}
 
 dependencies {
     testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
 }
 
 application {
