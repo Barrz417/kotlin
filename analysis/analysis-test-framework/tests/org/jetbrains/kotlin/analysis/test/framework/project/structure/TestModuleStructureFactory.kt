0c5dfb02cf5b875649d20123bb8c8bf373e8ed7d
Anton Lakotka <anton.lakotka@jetbrains.com>
2024-06-11 13:11:59 +0200

    [stdlib] Include kotlin-stdlib-common.klib to dist and stop publishing...
    ...legacy metadata variant.
    
    Update kotlin-compiler tests to use stdlib-common.klib artifact.
    
    ^KT-62159

--- analysis/analysis-test-framework/tests/org/jetbrains/kotlin/analysis/test/framework/project/structure/TestModuleStructureFactory.kt
+++ analysis/analysis-test-framework/tests/org/jetbrains/kotlin/analysis/test/framework/project/structure/TestModuleStructureFactory.kt
@@ -10,11 +10,7 @@ import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiManager
 import com.intellij.psi.search.GlobalSearchScope
 import org.jetbrains.kotlin.analysis.api.standalone.base.project.structure.StandaloneProjectFactory
-import org.jetbrains.kotlin.analysis.project.structure.KtBinaryModule
-import org.jetbrains.kotlin.analysis.project.structure.KtDanglingFileModule
-import org.jetbrains.kotlin.analysis.project.structure.KtLibraryModule
-import org.jetbrains.kotlin.analysis.project.structure.KtModule
-import org.jetbrains.kotlin.analysis.project.structure.KtNotUnderContentRootModule
+import org.jetbrains.kotlin.analysis.project.structure.*
 import org.jetbrains.kotlin.analysis.test.framework.AnalysisApiTestDirectives
 import org.jetbrains.kotlin.analysis.test.framework.services.environmentManager
 import org.jetbrains.kotlin.cli.common.CLIConfigurationKeys
@@ -171,7 +167,11 @@ object TestModuleStructureFactory {
             }
 
             for (libraryRoot in libraryRoots) {
-                check(libraryRoot.extension == "jar")
+                // Question from Anton Lakotka: Why this check is necessary? Is it used to ensure that classpath is not contaminated?
+                // For example for test: FirIdeNormalAnalysisSourceModuleCompilerFacilityTestGenerated$CodeFragments.testCommonContext
+                // the testModule can be common, and thus can receive non-jars
+                // but even for JVM we can pass unpacked classfiles. So I'll disable it for now.
+                //check(libraryRoot.extension == "jar")
 
                 val libraryModule = libraryCache(setOf(libraryRoot)) {
                     createLibraryModule(project, libraryRoot, JvmPlatforms.defaultJvmPlatform, testServices)
