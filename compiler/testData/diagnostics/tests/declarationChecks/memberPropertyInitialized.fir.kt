423d7ae6d6d14dbe0d8939c9c13febb46dd2a6ad
Brian Norman <brian.norman@jetbrains.com>
2024-06-26 10:19:02 -0500

    fixup! [FIR] Check for explicit type on initialized properties

--- compiler/testData/diagnostics/tests/declarationChecks/memberPropertyInitialized.fir.kt
+++ compiler/testData/diagnostics/tests/declarationChecks/memberPropertyInitialized.fir.kt
@@ -2,8 +2,13 @@
 
 class Clazz() {
     <!PROPERTY_WITH_NO_TYPE_NO_INITIALIZER!>var foo<!>
+    <!PROPERTY_WITH_NO_TYPE_NO_INITIALIZER!>var bar<!>
+        get() {
+            return 0
+        }
 
     init {
         foo = "hello"
+        bar = 0
     }
 }
